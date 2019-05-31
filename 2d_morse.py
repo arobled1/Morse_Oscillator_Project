9c53db215a718f7638143238d6b5ae9f1fa6c8b3
Alan Robledo <lanrobledo20@yahoo.com>
2019-05-30 18:51:17 -0700

    fixed bugs

--- 2d_morse.py
+++ 2d_morse.py
@@ -1,6 +1,6 @@
 #=============================================================================80
 # Description:
-# Computes an eigenvalue decomposition of the hamiltonian for a 1D morse
+# Computes an eigenvalue decomposition of the hamiltonian for a 2D morse
 # oscillator.
 #===============================================================================
 # Author: Alan Robledo
@@ -15,8 +15,9 @@
 # mass = reduced mass
 #===============================================================================
 import numpy as np
-from numpy import linalg as la
-import matplotlib.pyplot as plt
+from scipy import linalg as la
+import copy
+#import matplotlib.pyplot as plt
 
 def generate_grid(min, max, ngrid):
     dx = (max - min) / (ngrid - 1)
@@ -35,22 +36,22 @@ def get_kinetic(ngrid, dx):
         ke_matrix[i + 1][i] = 1
     return -(hbar**2 / (2.0 * mass)) * dx**-2 * ke_matrix
 
-def get_potential(ngrid):
+def get_potential(ngrid, d_well, omega):
     pe_matrix = np.zeros((ngrid, ngrid))
     for i in xrange(ngrid):
-        for j in xrange(ngrid):
-            pe_matrix[i][j] = d_well * ( (np.exp(-omegax * grid[i]) - 1)**2 + (np.exp(-omegay * grid[j]) - 1)**2)
+        pe_matrix[i][i] = d_well * (np.exp(-omega * grid[i]) - 1)**2
     return pe_matrix
 
 def bubble_sort(eig_energies, eig_vectors):
-    new_list = eig_energies[:]
+    new_list = copy.copy(eig_energies)
+    new_mat = copy.deepcopy(eig_vectors)
     num_pairs = len(new_list) - 1
     for j in xrange(num_pairs):
         for i in xrange(num_pairs - j):
             if new_list[i] > new_list[i+1]:
                 new_list[i], new_list[i+1] = new_list[i+1], new_list[i]
-                eig_vectors[:,i], eig_vectors[:,i+1] = eig_vectors[:,i+1], eig_vectors[:,i]
-    return new_list, eig_vectors
+                new_mat[:,[i, i+1]] = new_mat[:,[i+1,i]]
+    return new_list, new_mat
 
 d_well = 12
 hbar = 1
@@ -61,16 +62,23 @@ grid_min = -4.0
 grid_max = 12.0
 ngrid = 300
 
+# Defining the grid
 dx, grid = generate_grid(grid_min, grid_max, ngrid)
+# Getting kinetic energy matrix
 ke_matrix = get_kinetic(ngrid, dx)
-pe_matrix = get_potential(ngrid)
 # Kinetic energy in x is the same as in y so we can just multiply by 2.
-pe_matrix = 2 * pe_matrix
-hamiltonian = ke_matrix + pe_matrix
+ke_matrix = 2 * ke_matrix
+# Getting potential energy matrix
+pex_matrix = get_potential(ngrid, d_well, omegax)
+pey_matrix = get_potential(ngrid, d_well, omegay)
+# Making hamiltonian matrix
+hamiltonian = ke_matrix + pex_matrix + pey_matrix
+# Eigenvalue decomposition of hamiltonian
 eig_val, eig_vec = la.eig(hamiltonian)
-eig_val, eig_vec = bubble_sort(eig_val, eig_vec)
+# Sorting the eigenvalues and corresponding eigenvectors
+sort_eigval, sort_eigvec = bubble_sort(eig_val, eig_vec)
 f = open('2d_eigenvalues.dat', 'wb')
 f.write("n       E_n\n")
 for i in xrange(ngrid):
-    f.write("%s %s\n" % (i, eig_val[i]))
+    f.write("%s %s\n" % (i, sort_eigval[i]))
 f.close()
