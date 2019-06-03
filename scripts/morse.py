44af6bc6994a42231abf0963ece702b45dfd7647
Alan Robledo <lanrobledo20@yahoo.com>
2019-06-02 21:05:06 -0700

    changed file names

--- scripts/morse.py
+++ scripts/morse.py
@@ -107,7 +107,7 @@ plt.plot(x_grid, plot_vt + 0.3, label=r'V$^T$', color='black')
 plt.axhline(y = 0, linestyle='dashed', color='grey')
 plt.axhline(y = 0.3, linestyle='dashed', color='grey')
 plt.legend()
-plt.savefig("diditwork.pdf")
+plt.savefig("ntos.pdf")
 plt.clf()
 
 # Next 6 lines are solely for making plots of prob. densities
